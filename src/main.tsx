5b000074a9693093aef33142a1a51e3fc83e4166
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-04-24 15:40:14 +0600

    auth ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-// import { Provider } from "react-redux";
 import "./index.css";
 import AppRouter from "./routes/AppRouter";
+import { Provider } from "react-redux";
+import { store } from "./redux/features/store";
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <RouterProvider router={AppRouter} />
-    {/* </Provider> */}
+      <Toaster position="top-right" reverseOrder={false} />
+    </Provider>
   </React.StrictMode>
 );
